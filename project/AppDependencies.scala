76fd5918c92315ed5dc9a710151ceb6724278c58
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-08-25 09:48:58 +0100

    APIS-8305 Upgrade to latest hmrc play frontend (#119)
    * APIS-8305 Upgrade to latest hmrc play frontend
    
    * APIS-8305 PR bot actions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
-  private val hmrcMongoVersion = "2.12.0"
+  private val hmrcMongoVersion = "2.13.0"
   private val apiDomainVersion = "1.2.0"
   private val tpdDomainVersion = "0.15.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.32.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "13.11.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "commons-validator"       %  "commons-validator"                      % "1.9.0",
     "uk.gov.hmrc"             %% "http-metrics"                           % "2.9.0",
